98f3ac97fcc7bb963460781be70cbc8073290b60
MMpierre <120421335+MMpierre@users.noreply.github.com>
2023-02-24 15:08:01 +0100

    Update HMInterface2.py

--- HMInterface2.py
+++ HMInterface2.py
@@ -38,7 +38,7 @@ description = col2.empty()
 
 st.sidebar.image("logoGEN.png")
 
-with open('src/Visualization/style.css') as f:
+with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
